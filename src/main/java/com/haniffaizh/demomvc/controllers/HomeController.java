8087bdd3608e43554e66437204370024baece87c
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2024-04-03 21:05:39 +0700

    handling form submission

--- src/main/java/com/haniffaizh/demomvc/controllers/HomeController.java
+++ src/main/java/com/haniffaizh/demomvc/controllers/HomeController.java
@@ -1,10 +1,12 @@
 package com.haniffaizh.demomvc.controllers;
 
+import com.haniffaizh.demomvc.entity.Product;
 import com.haniffaizh.demomvc.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -21,6 +23,18 @@ public class HomeController {
         model.addAttribute("msg", messages);
         model.addAttribute("products", productService.findAll());
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String add(Model model){
+        model.addAttribute("product", new Product());
+        return "add";
+    }
+
+    @PostMapping("/save")
+    public String save(Product product){
+        productService.addProduct(product);
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/haniffaizh/demomvc/service/ProductService.java
+++ src/main/java/com/haniffaizh/demomvc/service/ProductService.java
@@ -3,22 +3,27 @@ package com.haniffaizh.demomvc.service;
 import com.haniffaizh.demomvc.entity.Product;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+
 import java.util.List;
+
 
 @Service
 public class ProductService {
 
-    private static List<Product> products = Arrays.asList(
-            new Product(1L, "001", "Product 001",1000.0),
-            new Product(2L, "002", "Product 002",2000.0),
-            new Product(3L, "003", "Product 003",3000.0),
-            new Product(4L, "004", "Product 004",4000.0),
-            new Product(5L, "005", "Product 005",5000.0)
-    );
+    private static List<Product> products = new ArrayList<Product>(){
+
+    };
+
+
 
     public List<Product> findAll(){
         return products;
+    }
+
+    public void addProduct(Product product){
+        product.setId(System.currentTimeMillis());
+       products.add(product);
     }
 
 }
